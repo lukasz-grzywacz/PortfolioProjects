29365450b5ca99e6757b7a8ca254c392a42bf079
Łukasz Grzywacz <86189662+xluki3@users.noreply.github.com>
2024-01-11 21:44:56 +0100

    Add files via upload

---
+++ Kaggle-PY-Snowflake-Project-Github/SF_validate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import snowflake.connector
+import json
+
+# Get the credentials
+config_location = 'path_to_SF_cred.json_file'
+
+config = json.loads(open(str(config_location+'/<credentials filename>')).read())
+
+username = config['secrets']['username']
+password = config['secrets']['password']
+account = config['secrets']['account']
+
+# Gets the version
+ctx = snowflake.connector.connect(
+    user        = username,
+    password    = password,
+    account     = account
+    )
+cs = ctx.cursor()
+try:
+    cs.execute("SELECT current_version()")
+    one_row = cs.fetchone()
+    print(one_row[0])
+finally:
+    cs.close()
+ctx.close()
